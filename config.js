6b35291ed0e7e9f9ae2b02c38a2823107d6c96a5
kevineshooter0-boop <kevineshooter0@gmail.com>
2026-09-19 19:36:27 +0100

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://xsokymzxxqkvacwzpzoa.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://xsokymzxxqkvacwzpzoa.supabase.co";
 const SUPABASE_KEY = "sb_publishable_vU5hDK2-ds81jnp74PCbgQ_SfBRqGZj";
